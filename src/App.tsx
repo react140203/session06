cc58a4b590ab55f3d9cdde090b1504d1d9c435ad
Pasha <pashaie@gmail.com>
2023-05-30 20:37:10 +0330

    src:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// website -> tailwind
+// webapp -> ant design
 import { Counter } from "./components/Counter";
-import { TaskList } from "./components/TaskList";
+import { TaskList } from "./components/task/TaskList";
 
 function App() {
   return (
