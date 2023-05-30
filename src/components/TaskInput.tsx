3cb5c3f86db12f6e1db1dd65448865f1b4616edb
Pasha <pashaie@gmail.com>
2023-05-30 19:32:21 +0330

    components/TaskInput:

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 
 export const TaskInput = ({ add }: any) => {
   const [newTaskTitle, setNewTaskTitle] = useState("");
+  const addFnc = () => {
+    add(newTaskTitle);
+    setNewTaskTitle("");
+  };
   return (
     <>
       <input
         value={newTaskTitle}
         onChange={(e) => setNewTaskTitle(e.target.value)}
       ></input>
-      <button onClick={() => add(newTaskTitle)}>➕</button>
+      <button onClick={addFnc}>➕</button>
     </>
   );
 };
