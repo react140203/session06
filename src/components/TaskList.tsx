15f1d3b9c7388aa1e855282da2e0ea08ba347e4e
Pasha <pashaie@gmail.com>
2023-05-30 17:57:25 +0330

    components/TaskList:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,22 @@
 //rafc <- snippet es7+
 import React from "react";
 
-const taskList = [{}];
+const taskList = [
+  { id: 1, title: "One", done: true },
+  { id: 2, title: "iki", done: false },
+  { id: 3, title: "se", done: false },
+  { id: 4, title: "quad", done: true },
+];
 
 export const TaskList = () => {
-  return <div>TaskList</div>;
-};
-
-export const TaskList2 = () => {
-  return <div>TaskList 2</div>;
+  return (
+    <>
+      <div>TaskList</div>
+      <ul>
+        {taskList.map((x) => (
+          <li>{x.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
