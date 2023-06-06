2bb6eba500312c18269bc3ef8e9b21907ea9e274
Pasha <pashaie@gmail.com>
2023-06-06 18:59:10 +0330

    components/post/PostList:

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -7,6 +7,7 @@ export const PostList = () => {
   const [posts, setPosts] = useState<any>([]);
   const [total, setTotal] = useState(1);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   /*
         Ajax
             fetch
@@ -33,12 +34,14 @@ export const PostList = () => {
     // async/await
 
     async function loadData() {
+      setLoading(true);
       const resp = await fetch(
         `https://jsonplaceholder.ir/posts?_limit=10&_page=${page}`
       );
       const json = await resp.json();
 
       //   setTotal(parseInt(resp.headers.get('X-Total-Count') || '0'));
+      setLoading(false);
       setTotal(+(resp.headers.get("X-Total-Count") || "0"));
       setPosts(json);
     }
@@ -51,16 +54,6 @@ export const PostList = () => {
     };
   }, [page]); //dependency
 
-  // const pages = [];
-  // for (let i = 1; i <= total / 10; i++) {
-  //   pages.push(
-  //     <li className={page === i ? "page-item active" : "page-item"}>
-  //       <a className="page-link" onClick={() => setPage(i)} href="#">
-  //         {i}
-  //       </a>
-  //     </li>
-  //   );
-  // }
   const columns: ColumnsType<any> = [
     {
       title: "Id",
@@ -86,58 +79,17 @@ export const PostList = () => {
   return (
     <>
       <div>PostList</div>
-      posts: {posts.length}
-      <nav aria-label="Page navigation example">
-        <Pagination
-          defaultCurrent={1}
-          total={total}
-          onChange={(page) => setPage(page)}
-        />
-        {/* <ul className="pagination">
-          <li className="page-item">
-            <a className="page-link" href="#">
-              Previous
-            </a>
-          </li>
-          {total > 10 &&
-            new Array(total / 10).fill(0).map((x, i) => (
-              <li className={page === i + 1 ? "page-item active" : "page-item"}>
-                <a
-                  className="page-link"
-                  onClick={() => setPage(i + 1)}
-                  href="#"
-                >
-                  {i + 1}
-                </a>
-              </li>
-            ))}
-          <li className="page-item">
-            <a className="page-link" href="#">
-              Next
-            </a>
-          </li>
-        </ul> */}
-      </nav>
-      {/* <table className="table table-striped">
-        <thead>
-          <th>id</th>
-          <th>userId</th>
-          <th>title</th>
-          <th>body</th>
-        </thead>
-        <tbody>
-          {posts.map((post: any) => (
-            <tr>
-              <td>{post.id}</td>
-              <td>{post.userId}</td>
-              <td>{post.title.substring(0, 20)} ...</td>
-              <td>{post.body.substring(0, 20)} ...</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-      <Table columns={columns} dataSource={posts} pagination={false} />
-      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+      <Pagination
+        defaultCurrent={1}
+        total={total}
+        onChange={(page) => setPage(page)}
+      />
+      <Table
+        columns={columns}
+        dataSource={posts}
+        pagination={false}
+        loading={loading}
+      />
     </>
   );
 };
