602e7025f0e7efe9de9c501c58c6f0aa92a36fc5
Pasha <pashaie@gmail.com>
2023-06-20 18:30:24 +0330

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import "./App.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // website -> tailwind
 // webapp -> ant design
-import { Counter } from "./components/Counter";
-import { TaskList } from "./components/task/TaskList";
-import { PostList } from "./components/pages/PostList";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { PhotoList } from "./components/pages/PhotoList";
-import { SelectColor } from "./components/SelectColor";
 import { AppContext } from "./appContext";
 import { AppLayout } from "./components/AppLayout";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
   const [color, setColor] = useState("red");
   return (
     <>
