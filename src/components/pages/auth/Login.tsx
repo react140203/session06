d56279f1dbc1e5286a089da4a592b61a07d6b2c6
Pasha <pashaie@gmail.com>
2023-06-27 20:32:32 +0330

    login

--- src/components/pages/auth/Login.tsx
+++ src/components/pages/auth/Login.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { Button, Checkbox, Form, Input, Layout } from "antd";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <>
+      <Layout>
+        <Layout.Content style={{ minHeight: "500px", textAlign: "center" }}>
+          <Form
+            name="basic"
+            style={{ maxWidth: 600 }}
+            onFinish={onFinish}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                  type: "email",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Layout.Content>
+      </Layout>
+    </>
+  );
 };
