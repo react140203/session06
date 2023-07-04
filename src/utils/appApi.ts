ff857b6d8d67a0579011afaa6fe3f0304858713e
Pasha <pashaie@gmail.com>
2023-07-04 18:47:44 +0330

    error handeling

--- src/utils/appApi.ts
+++ src/utils/appApi.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { store } from "../redux/store";
+import { notification } from "antd";
 
 export const appApi = axios.create({
   baseURL: "http://localhost:3010",
@@ -13,5 +14,23 @@ appApi.interceptors.request.use((config) => {
   }
   return config;
 });
+
+appApi.interceptors.response.use(
+  (response) => {
+    if (response?.data?.message) {
+      notification.success({ message: response.data.message });
+    }
+    return Promise.resolve(response);
+  },
+  (error) => {
+    console.log(error.response.data);
+    if (error.response?.data?.error) {
+      notification.error({ message: error.response.data.error });
+    } else {
+      notification.error({ message: error.message });
+    }
+    return Promise.reject(error);
+  }
+);
 
 //
